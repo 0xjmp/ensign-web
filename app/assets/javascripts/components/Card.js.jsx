02ec177ee98fe46e768586286c221810818a720a
Jake Peterson <jsksma2@gmail.com>
2015-08-04 17:48:32 -0700

    Cards.jsx: clean up implementation

--- app/assets/javascripts/components/Card.js.jsx
+++ app/assets/javascripts/components/Card.js.jsx
@@ -4,23 +4,13 @@ var Card = React.createClass({
     type: React.PropTypes.oneOf(['candidate', 'employer']),
     card: React.PropTypes.object
   },
-  componentDidMount: function() {
-    this.props.stack.createCard(React.findDOMNode(this.refs.card));
-  },
-  componenetWillUnmount: function() {
-    this.destroyCard();
-  },
-  destroyCard: function() {
-    var card = this.props.stack.getCard(this.refs.card);
-    card.destroy();
-  },
   render: function() {
     switch (this.props.type) {
       case 'candidate':
-        return <Candidate candidate={this.props.card} ref="card" />;
+        return <Candidate candidate={this.props.card} />;
 
       case 'employer':
-        return <Employer employer={this.props.card} ref="card" />;
+        return <Employer employer={this.props.card} />;
 
       default:
         break;
--- app/assets/javascripts/components/Cards.js.jsx
+++ app/assets/javascripts/components/Cards.js.jsx
@@ -8,22 +8,29 @@ var Cards = React.createClass({
       stack: gajus.Swing.Stack()
     }
   },
-  componentDidMount: function() {
+  componentWillMount: function() {
     this.state.stack.on('throwout', this.handleThrowout);
+  },
+  componentWillUnmount: function() {
+    debugger;
   },
   handleThrowout: function(element) {
     console.log('Card has been thrown out of stack');
+  },
+  setupCard: function(component) {
+    this.state.stack.createCard(React.findDOMNode(component));
   },
   render: function() {
     var type = this.props.cardType;
     var current = this.props.currentCard;
     var stack = this.state.stack;
+    var setupCard = this.setupCard;
     return (
       <div>
         <ul className="cards-wrapper">
             {this.props.cards.map(function(card) {
               return (
-                <li key={card.id} className="card">
+                <li key={card.id} className="card" ref={setupCard}>
                   <Card card={card} type={type} stack={stack} />
                 </li>
               );
